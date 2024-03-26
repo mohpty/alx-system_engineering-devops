ec8df47402e00653ed7918ce363ff9cc7f8e6e23
Mohammad Fathi <mohfabdou@gmail.com>
2024-03-26 23:10:51 +0300

    fixing 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,13 +1,7 @@
-# Kill a process called 'killmenow'
-
-# Only kill the process if it exist
-
-# If it doesn't exists, puppet should exit
-
-# with a 0 return code and not 1
+# kill process killmenow 
 
 
-exec {'kill `killmenow` process':
-command => '/usr/bin/pkill -9 -f killmenow',
-onlyif  => '/usr/bin/pgrep -f killmenow'
+exec {'pkill':
+command => '/usr/bin/pkill killmenow',
+provider => 'shell'
 }
